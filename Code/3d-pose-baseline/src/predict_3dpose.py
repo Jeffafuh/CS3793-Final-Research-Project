e1f77358d65c47f9d2f98406c626f29a51cdab78
Jeff Dong <85254367+Jeffafuh@users.noreply.github.com>
2022-08-07 19:34:16 -0500

    Update tf functions

--- Code/3d-pose-baseline/src/predict_3dpose.py
+++ Code/3d-pose-baseline/src/predict_3dpose.py
@@ -15,41 +15,41 @@ import linear_model
 import procrustes
 import viz
 
-tf.app.flags.DEFINE_float("learning_rate", 1e-3, "Learning rate")
-tf.app.flags.DEFINE_float("dropout", 1, "Dropout keep probability. 1 means no dropout")
-tf.app.flags.DEFINE_integer("batch_size", 64, "Batch size to use during training")
-tf.app.flags.DEFINE_integer("epochs", 200, "How many epochs we should train for")
-tf.app.flags.DEFINE_boolean("camera_frame", False, "Convert 3d poses to camera coordinates")
-tf.app.flags.DEFINE_boolean("max_norm", False, "Apply maxnorm constraint to the weights")
-tf.app.flags.DEFINE_boolean("batch_norm", False, "Use batch_normalization")
+tf.compat.v1.flags.DEFINE_float("learning_rate", 1e-3, "Learning rate")
+tf.compat.v1.flags.DEFINE_float("dropout", 1, "Dropout keep probability. 1 means no dropout")
+tf.compat.v1.flags.DEFINE_integer("batch_size", 64, "Batch size to use during training")
+tf.compat.v1.flags.DEFINE_integer("epochs", 200, "How many epochs we should train for")
+tf.compat.v1.flags.DEFINE_boolean("camera_frame", False, "Convert 3d poses to camera coordinates")
+tf.compat.v1.flags.DEFINE_boolean("max_norm", False, "Apply maxnorm constraint to the weights")
+tf.compat.v1.flags.DEFINE_boolean("batch_norm", False, "Use batch_normalization")
 
 # Data loading
-tf.app.flags.DEFINE_boolean("predict_14", False, "predict 14 joints")
-tf.app.flags.DEFINE_string("action","All", "The action to train on. 'All' means all the actions")
+tf.compat.v1.flags.DEFINE_boolean("predict_14", False, "predict 14 joints")
+tf.compat.v1.flags.DEFINE_string("action","All", "The action to train on. 'All' means all the actions")
 
 # Architecture
-tf.app.flags.DEFINE_integer("linear_size", 1024, "Size of each model layer.")
-tf.app.flags.DEFINE_integer("num_layers", 2, "Number of layers in the model.")
-tf.app.flags.DEFINE_boolean("residual", False, "Whether to add a residual connection every 2 layers")
+tf.compat.v1.flags.DEFINE_integer("linear_size", 1024, "Size of each model layer.")
+tf.compat.v1.flags.DEFINE_integer("num_layers", 2, "Number of layers in the model.")
+tf.compat.v1.flags.DEFINE_boolean("residual", False, "Whether to add a residual connection every 2 layers")
 
 # Evaluation
-tf.app.flags.DEFINE_boolean("procrustes", False, "Apply procrustes analysis at test time")
-tf.app.flags.DEFINE_boolean("evaluateActionWise", False, "The dataset to use either h36m or heva")
+tf.compat.v1.flags.DEFINE_boolean("procrustes", False, "Apply procrustes analysis at test time")
+tf.compat.v1.flags.DEFINE_boolean("evaluateActionWise", False, "The dataset to use either h36m or heva")
 
 # Directories
-tf.app.flags.DEFINE_string("cameras_path","data/h36m/metadata.xml", "File with h36m metadata, including cameras")
-tf.app.flags.DEFINE_string("data_dir",   "data/h36m/", "Data directory")
-tf.app.flags.DEFINE_string("train_dir", "experiments", "Training directory.")
+tf.compat.v1.flags.DEFINE_string("cameras_path","data/h36m/metadata.xml", "File with h36m metadata, including cameras")
+tf.compat.v1.flags.DEFINE_string("data_dir",   "data/h36m/", "Data directory")
+tf.compat.v1.flags.DEFINE_string("train_dir", "experiments", "Training directory.")
 
 # Train or load
-tf.app.flags.DEFINE_boolean("sample", False, "Set to True for sampling.")
-tf.app.flags.DEFINE_boolean("use_cpu", False, "Whether to use the CPU")
-tf.app.flags.DEFINE_integer("load", 0, "Try to load a previous checkpoint.")
+tf.compat.v1.flags.DEFINE_boolean("sample", False, "Set to True for sampling.")
+tf.compat.v1.flags.DEFINE_boolean("use_cpu", False, "Whether to use the CPU")
+tf.compat.v1.flags.DEFINE_integer("load", 0, "Try to load a previous checkpoint.")
 
 # Misc
-tf.app.flags.DEFINE_boolean("use_fp16", False, "Train using fp16 instead of fp32.")
+tf.compat.v1.flags.DEFINE_boolean("use_fp16", False, "Train using fp16 instead of fp32.")
 
-FLAGS = tf.app.flags.FLAGS
+FLAGS = tf.compat.v1.flags.FLAGS
 
 train_dir = os.path.join( FLAGS.train_dir,
   FLAGS.action,
